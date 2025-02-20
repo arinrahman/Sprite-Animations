af7c0bf456a91aa2386aa59be76c9b2e39b15826
arinrahman <rahman.arin29@gmail.com>
2025-02-19 19:16:40 -0700

    return a 2D drawing context

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 const canvas= document.getElementById("canvas1");
+/* returns a 2D drawing context and 
+allows to draw 2 dimentional 
+shapes, text, images, and animations */
+const context= canvas.getContext("2d"); 
