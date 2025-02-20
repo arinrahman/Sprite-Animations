8f31baba5cdc6ee9ab6b73a6ec57c6efa34fa9ce
arinrahman <rahman.arin29@gmail.com>
2025-02-19 19:58:13 -0700

    new Image – built in JS consructor to bring image in .js file

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const context= canvas.getContext("2d");
 /* adding canvas height and width*/
 const CANVAS_WIDTH= canvas.width= 600;
 const CANVAS_HEIGHT= canvas.height= 600;
+
+/* console.log(context)*/
+/*to inspect*/
+/*we'll focus more on the drawImage method */
+
+
+/*to bring image in the spript.js file I will 
+declare a custom variable called playerImage*/
+
+/*new Image() allows me to create a new image object 
+that will allow me to load image and modify it*/
+
+/*new Image is a built in JS constructor
+it is a short form for 
+const playerImage=document.createElement("img")
+*/
+
+
+const playerImage= new Image();
+/*
+*/
+
+
