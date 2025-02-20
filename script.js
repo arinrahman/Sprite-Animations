efa703565db261e9c4e20ad84d5c0ab399c04810
arinrahman <rahman.arin29@gmail.com>
2025-02-19 19:20:06 -0700

    created to variables to set canvas height and width

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const canvas= document.getElementById("canvas1");
 allows to draw 2 dimentional 
 shapes, text, images, and animations */
 const context= canvas.getContext("2d"); 
+/* adding canvas height and width*/
+const CANVAS_WIDTH= canvas.width= 600;
+const CANVAS_HEIGHT= canvas.height= 600;
